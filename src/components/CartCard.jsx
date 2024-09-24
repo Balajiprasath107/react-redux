274b552a1f59d858e79ecb082e44fda83c6263cd
Balajiprasath T <balajiprasath107@gmail.com>
2024-09-24 20:43:22 +0530

    import selector and dispatches

--- src/components/CartCard.jsx
+++ src/components/CartCard.jsx
@@ -1,44 +1,45 @@
-import React, { useContext, useState } from 'react'
+import React from 'react'
 import './style.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { cartremove, newcartCount, newtotal } from '../redux/reducer/cart.reducer'
+import { changeincart, changequantity } from '../redux/reducer/products.reducer'
 function CartCard({ item }) {
-    const { removeitem, cartCount, setCartCount,TotalCartPrice,setTotalCartPrice} = useContext(CartContext)
-    const [quantity, setquantity] = useState(item.quantity)
-
-
-
+    const dispatch = useDispatch()
+    const {quantity} = useSelector((state) => state.products)
+    const {cartCount,TotalCartPrice} = useSelector((state) => state.cart)
     function handleChange(e) {
         let newquantity = Number(e.target.value)
         let newTotalCart = (Math.round((TotalCartPrice*100)-(item.price*item.quantity*100)+(item.price*newquantity*100))/100)
-        setTotalCartPrice(newTotalCart)
-        let newCount = cartCount - item.quantity + newquantity
-        item.quantity = newquantity
-        setquantity(item.quantity)
-        setCartCount(newCount)
+        dispatch(newtotal(newTotalCart))
+        let newCount = cartCount - quantity[item.id-1] + newquantity
+        dispatch(changequantity([item.id,newquantity]))
+        dispatch(newcartCount(newCount))
     }
 
 
     return (
+       
         <div className='cartItem'>
             <div className='cartItemCard'>
                 <img className='cartItemCard-image ' src={item.image} alt="" />
                 <div className='cartItemDetails'>
-                    <h3>{item.title}</h3>
+                    <h3 style={{height : "65px", overflow:"clip"}}>{item.title}</h3>
                     <div className="d-flex small text-warning mb-2">
                             {
-                                [...Array(Math.ceil(item.rating.rate))].map((ele, index) =>
-                                    <div className="bi-star-fill"></div>
+                                [...Array(Math.round(item.rating))].map((ele, index) =>
+                                    <div className="bi-star-fill" key={item.id+"star"+index}></div>
 
                                 )
 
                             }
                             {
-                                [...Array(5 - Math.ceil(item.rating.rate))].map((ele, index) =>
-                                    <div className="bi-star"></div>
+                                [...Array(5 - Math.round(item.rating))].map((ele, index) =>
+                                    <div className="bi-star" key={item.id+"nostar"+index}></div>
 
                                 )
                             }
-                            <div className="ms-2" style={{fontWeight:"bold"}}>{item.rating.rate}</div>
-                            <div className="ms-2" style={{color:"#007185", fontWeight:"bold"}}>{item.rating.count}</div>
+                            <div className="ms-2" style={{fontWeight:"bold"}}>{item.rating}</div>
+                            <div className="ms-2" style={{color:"#007185", fontWeight:"bold"}}>{item.count}</div>
                         </div>
                     <h5 className='text-decoration-underline'>Description</h5>
                     <p className='description'>{item.description}</p>
@@ -48,7 +49,7 @@ function CartCard({ item }) {
             <div className='priceDetails'>
                 <div className='quantityAndPrice'>
                     <label htmlFor='quantity'>Quantity</label>
-                    <select name="quantity" id="" value={quantity} onChange={handleChange}>
+                    <select name="quantity" id="" value={quantity[item.id-1]} onChange={handleChange}>
                         <option value="1">1</option>
                         <option value="2">2</option>
                         <option value="3">3</option>
@@ -63,10 +64,15 @@ function CartCard({ item }) {
 
                 </div>
                 <p>price: ${item.price}</p>
-                <h5>Total price: ${item.price * quantity}</h5>
+                <h5>Total price: ${item.price * quantity[item.id-1]}</h5>
                 <button className="removebtw btn btn-danger m-2" onClick={() => {
-                    removeitem(item)
-                    item.incart = !(item.incart)
+                    let newcount = cartCount - quantity[item.id-1]
+                    dispatch(newcartCount(newcount))
+                    let newTotalCart = Math.round((TotalCartPrice*100)-((item.price*100)*quantity[item.id-1]))/100
+                    dispatch(newtotal(newTotalCart))    
+                    dispatch(changequantity([item.id,1]))
+                    dispatch(changeincart(item.id))
+                    dispatch(cartremove(item))
                 }
                 }>Remove</button>
             </div>
