62efe41509054cc537dd789a2919f74868044306
Balajiprasath T <balajiprasath107@gmail.com>
2024-09-24 19:48:24 +0530

    new state quantity

--- src/redux/reducer/products.reducer.js
+++ src/redux/reducer/products.reducer.js
@@ -7,7 +7,8 @@ export const productSlice =  createSlice(
         initialState : {
             isLoading : false,
             products : products,
-            incart : Array(products.length).fill(false)
+            incart : Array(products.length).fill(false),
+            quantity : Array(products.length).fill(1)
         },
         reducers : {
             saveLAllProducts : (state,action) => {
@@ -16,7 +17,6 @@ export const productSlice =  createSlice(
             },
             changeincart : (state,action) => {
                 state.incart[action.payload-1] = (!state.incart[action.payload-1])
-                console.log(state.incart[action.payload-1],action.payload)
             }
         }
     }
