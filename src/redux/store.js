ff6d0a29e4eea7648153f4541a86e986fdd875ea
Balajiprasath T <balajiprasath107@gmail.com>
2024-09-24 17:30:00 +0530

    additional reducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productsReducer from "./reducer/products.reducer";
+import  cartReducer from "./reducer/cart.reducer";
 
 export default configureStore(
     {
         //root reducer
         reducer :{
-            products : productsReducer
+            products : productsReducer,
+            cart : cartReducer
         }
     }
 )
