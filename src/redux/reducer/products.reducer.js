9ddf6fcf8c3140e8b7700fbc267b04aa11a5be62
Balajiprasath T <balajiprasath107@gmail.com>
2024-09-24 19:43:54 +0530

    new states and reducers

--- src/redux/reducer/products.reducer.js
+++ src/redux/reducer/products.reducer.js
@@ -1,22 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
-
+import products from '../../product.json'
 //create slice
 export const productSlice =  createSlice(
     {
         name : 'products',
         initialState : {
             isLoading : false,
-            products : []
+            products : products,
+            incart : Array(products.length).fill(false)
         },
         reducers : {
             saveLAllProducts : (state,action) => {
                 console.log(state,action);
                 
+            },
+            changeincart : (state,action) => {
+                state.incart[action.payload-1] = (!state.incart[action.payload-1])
+                console.log(state.incart[action.payload-1],action.payload)
             }
         }
     }
 )
-
-export const {saveLAllProducts} = productSlice.actions;
+export const {saveLAllProducts,changeincart} = productSlice.actions;
 
 export default productSlice.reducer;
