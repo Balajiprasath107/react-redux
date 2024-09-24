1666109973f4a701aa818437985ac5debd225927
Balajiprasath T <balajiprasath107@gmail.com>
2024-09-24 17:29:26 +0530

    created routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { createBrowserRouter, Routes, Route, Link, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+import Routelayout from './components/RouteLayout'
+import Home from './components/Home'
+import About from './components/About'
+import Cart from './components/Cart'
 
 function App() {
   
-  
+  const router =createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Routelayout/>}>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='about' element={<About/>}></Route>
+        <Route path='cart' element={<Cart/>}></Route>
+      </Route>
+
+    )
+  )
   return (
-    <>
-     
-    </>
+      <RouterProvider router={router} />
   )
 }
 
